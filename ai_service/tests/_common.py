97c79ec2cb2766d20bf12489d1700ede0ae0098e
Intarn <quochoc112@gmail.com>
2026-08-10 21:37:04 +0700

    Delete ai_service/tests/_common.py

--- ai_service/tests/_common.py
+++
@@ -1,7 +0,0 @@
-# _common.py
-with open("token.txt") as f:
-    TOKEN = f.read().strip()
-
-HEADERS = {"Authorization": f"Bearer {TOKEN}"}
-BASE_URL = "http://localhost:5000/api/ai"
-PROJECT_ID = "dcea2e78-0ffd-4c46-bf9b-12bcb65de983"
